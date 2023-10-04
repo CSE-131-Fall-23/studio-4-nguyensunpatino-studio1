867892536aa088ceb0e802af35e8022f357cc7cd
saoren190 <59161429+saoren190@users.noreply.github.com>
2023-10-04 14:15:35 -0500

    studio4

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,22 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
-		
-	}
+		int canvasWidth = 900;
+		int canvasHeight = 600;
+		StdDraw.setCanvasSize(canvasWidth, canvasHeight);
+	    StdDraw.setXscale(0, canvasWidth);
+	    StdDraw.setYscale(0, canvasHeight);
+
+	        // Draw the left band (color: blue)
+	    StdDraw.setPenColor(StdDraw.BLACK);
+	    StdDraw.filledRectangle(canvasWidth / 6, canvasHeight / 2, canvasWidth / 6, canvasHeight / 2);
+
+	        // Draw the middle band (color: white)
+	    StdDraw.setPenColor(StdDraw.WHITE);
+	    StdDraw.filledRectangle(canvasWidth / 2, canvasHeight / 2, canvasWidth / 6, canvasHeight / 2);
+
+	        // Draw the right band (color: red)
+	     StdDraw.setPenColor(StdDraw.RED);
+	     StdDraw.filledRectangle(5 * canvasWidth / 6, canvasHeight / 2, canvasWidth / 6, canvasHeight / 2);
+	  }
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -20,5 +20,35 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		String shapeType = in.next();
+
+        if ("rectangle".equals(shapeType)) {
+            StdDraw.clear();
+            StdDraw.setCanvasSize(800, 600);
+
+            int redCOmponent = in.nextInt();
+            int greenCOmponent = in.nextInt();
+            int bileCOmponent = in.nextInt();
+            
+            
+            
+            boolean isFilled = in.nextBoolean();
+            double centerX = in.nextDouble();
+            double centerY = in.nextDouble();
+            
+            StdDraw.setXscale(0, centerX);
+            StdDraw.setYscale(0, centerY);
+            
+            double halfWidth = in.nextDouble();
+            double halfHeight = in.nextDouble();
+            
+            if (isFilled) {
+                StdDraw.filledRectangle(centerX, centerY, halfWidth, halfHeight);
+            } else {
+                StdDraw.rectangle(centerX, centerY, halfWidth, halfHeight);
+            }
+        }else {
+            System.err.println("Unsupported shape type: " + shapeType);
+        }
 	}
 }
